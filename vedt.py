fc83ce3558e7c95ab24aa000f6e16445adedb81a
guilhembn <gbuisan@laas.fr>
2018-10-11 10:26:21 +0200

    Adds exercices

---
+++ vedt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#-------------------
+# x = (longitudeB - longitudeA) * cos((latitudeA + latitudeB)/2)
+# y = (latitudeB - latitudeA)
+# d = sqrt(x**2 + y**2) * 6371
+
+if __name__ == '__main__':
+  # Read the "defibrillateurs.csv" file
+  
+  # Transform the lines in sport equipments tuples
+
+  # Get user input coordinates
+
+  # Find the closest tuple
+
+  # Prints its address
+
+  # Bonus: Loop it !
