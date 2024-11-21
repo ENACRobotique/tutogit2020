9335796c0ab545187681d1ae38573992543142f6
GUNDAM <robotique.enac@gmail.com>
2024-11-21 15:51:57 +0100

    -Add pain

--- bakery_sim/main.py
+++ bakery_sim/main.py
@@ -5,6 +5,7 @@ from produit import Produit, Croissant, Pain
 if __name__ == '__main__':
     b = Boulangerie()
     b.addProduit(Croissant())
+    b.addProduit(Pain())
 
     c = b.get_croissant()
     print(c)
