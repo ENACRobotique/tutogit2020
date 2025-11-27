d4eb3a68238c7151e8789f07c3ac7a6888bd28c6
minorant <arthurmorgan@gmail.com>
2025-11-27 10:03:18 +0100

    main

--- bakery_sim/main.py
+++ bakery_sim/main.py
@@ -1,11 +1,19 @@
 
 from boulangerie import Boulangerie
-from produit import Produit, Croissant, Pain
+from produit import Produit, Croissant, Pain, Chocolatine
 
 if __name__ == '__main__':
     b = Boulangerie()
-    b.addProduit(Croissant())
+    for _ in range(10):
+      b.addProduit(Croissant())
+      b.addProduit(Chocolatine())
+      b.addProduit(Pain())
+      b.addProduit(Pain())
+    
+
 
     c = b.get_croissant()
+
+    paint_au_chocolat = b.get_chocolatine()
     print(c)
     
